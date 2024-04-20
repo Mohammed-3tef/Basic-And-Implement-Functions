d5530c7a612cf931903911c7967bea3bc6547aef
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-20 14:01:09 +0200

    Update Range Sum.cpp

--- Range Sum.cpp
+++ Range Sum.cpp
@@ -4,13 +4,18 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ld long double
-#define NO cout << "NO" << endl;
-#define YES cout << "YES" << endl;
+#define all(x) (x).begin(), (x).end()                        // Sort The String From The Beginning.
+#define rall(x) (x).rbegin(), (x).rend()                        // Sort The String From The End.
+#define from_begin(array, num) sort(array, (array+num))                // Sort the array in ascending order.
+#define from_end(array, num) sort((array), (array+num), greater<ll>())      // Sort the array in descending order.
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+const ll MAX_CHAR = 256;
 using namespace std;
 
 int main() {
-    ll test;
-    cin >> test;
+    ll test; cin >> test;
     while (test--){
         ll t, tt, sum = 0;
         cin >> t >> tt;
