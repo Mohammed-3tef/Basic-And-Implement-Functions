339e5da68b84f6f2758adc31e6c4749cf0cc5cf4
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-20 14:00:37 +0200

    Update Multiply the matrix by the scalar.cpp

--- Multiply the matrix by the scalar.cpp
+++ Multiply the matrix by the scalar.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define ld long double
+#define all(x) (x).begin(), (x).end()                        // Sort The String From The Beginning.
+#define rall(x) (x).rbegin(), (x).rend()                        // Sort The String From The End.
+#define from_begin(array, num) sort(array, (array+num))                // Sort the array in ascending order.
+#define from_end(array, num) sort((array), (array+num), greater<ll>())      // Sort the array in descending order.
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+const ll MAX_CHAR = 256;
 using namespace std;
 
 void read_matrix(ll row, ll column, int** matrix)           // Get the matrix from the user.
