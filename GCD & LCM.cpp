69a93850332ee48add372703a69e56d51d205cd3
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-20 13:58:27 +0200

    Update GCD & LCM.cpp

--- GCD & LCM.cpp
+++ GCD & LCM.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ld long double
-#define NO cout << "NO" << endl;
-#define YES cout << "YES" << endl;
+#define all(x) (x).begin(), (x).end()                        // Sort The String From The Beginning.
+#define rall(x) (x).rbegin(), (x).rend()                        // Sort The String From The End.
+#define from_begin(array, num) sort(array, (array+num))                // Sort the array in ascending order.
+#define from_end(array, num) sort((array), (array+num), greater<ll>())      // Sort the array in descending order.
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+const ll MAX_CHAR = 256;
 using namespace std;
 
 // Recursive function to return gcd of a and b.
@@ -18,8 +24,7 @@ long long lcm(ll a, ll b){
 }
 
 int main() {
-    ll n1, n2;
-    cin >> n1 >> n2;
+    ll n1, n2; cin >> n1 >> n2;
     cout << gcd(n1, n2) << " " << lcm(n1, n2) << endl;
     return 0;
 }
