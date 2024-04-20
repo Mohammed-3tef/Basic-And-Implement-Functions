5651efa127c2aa4fd4bceceac64e15b26280a49f
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-20 13:59:03 +0200

    Update Is Prime ?.cpp

--- Is Prime ?.cpp
+++ Is Prime ?.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define all(x) (x).begin(), (x).end()                        // Sort The String From The Beginning.
+#define rall(x) (x).rbegin(), (x).rend()                        // Sort The String From The End.
+#define from_begin(array, num) sort(array, (array+num))                // Sort the array in ascending order.
+#define from_end(array, num) sort((array), (array+num), greater<ll>())      // Sort the array in descending order.
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+const ll MAX_CHAR = 256;
 using namespace std;
 
 int main(){
-    int n;
+    ll n;
     cout << "Please input a positive integer to see if prime: ";
     cin >> n;
 
