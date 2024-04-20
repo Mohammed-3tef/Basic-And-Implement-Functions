6c408ed8500f8b956955b16991f3c8bd04518bca
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-20 14:03:53 +0200

    Update Two Intervals.cpp

--- Two Intervals.cpp
+++ Two Intervals.cpp
@@ -1,9 +1,16 @@
-/*
-  https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/X
-*/
+// Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/X
+// =================================================================================== //
 
 #include <bits/stdc++.h>
 #define ll long long
+#define ld long double
+#define all(x) (x).begin(), (x).end()                        // Sort The String From The Beginning.
+#define rall(x) (x).rbegin(), (x).rend()                        // Sort The String From The End.
+#define from_begin(array, num) sort(array, (array+num))                // Sort the array in ascending order.
+#define from_end(array, num) sort((array), (array+num), greater<ll>())      // Sort the array in descending order.
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
 
 int main()
