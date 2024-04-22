33b084009246255dac2ebd55a1e1914db3d5de05
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-22 17:15:35 +0200

    Primes from 1 to n.cpp

--- Primes from 1 to n.cpp
+++ Primes from 1 to n.cpp
@@ -8,37 +8,28 @@
 #define no cout << "NO" << endl;
 #define yes cout << "YES" << endl;
 #define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-const ll MAX_CHAR = 256;
 using namespace std;
 
-int main(){
-    ll n;
-    cout << "Please input a positive integer to see if prime: ";
-    cin >> n;
-
-    while (n < 0) {
-        cout << "Please input a positive integer to see if prime: ";
-        cin >> n;
-    }
-    cout << endl;
-
-    // Cases 0 and 1 are not prime
-    if (n == 0 || n == 1) {
-        cout << "It's not a prime number.\n";
-        return 0;
-    }
-    else if (n == 2) {
-        cout << "It's a prime number.\n";
-        return 0;
-    } 
-    else {
-        for (int i = 2; i <= sqrt(n); i++) {
-            if (n % i == 0) {
-                cout << "It's not a prime number.\n";
-                return 0;
+int main() {
+    Hero
+    ll test = 1;
+//    cin >> test;
+    while(test--){
+        ll num; cin >> num;
+        ll arr[num];
+        
+        for (int i = 0; i < num; ++i) {
+            arr[i] = i+1;
+            if (arr[i] == 2) {cout << arr[i] << " ";}
+            else {
+                ll sum = 0;
+                for (int j = 1; j <= arr[i]; j++) {
+                    if (arr[i] % j == 0) {sum++;}
+                }
+                if (sum == 2) {cout << arr[i] << " ";}
             }
         }
-        cout << "It's a prime number.\n";
+        
     }
     return 0;
 }
