21eaae5c91d6d3568a25b8f4d481b1cc6cc9e974
Mohammed Atef Abd El-Kader <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-04-22 19:36:19 +0200

    Update and rename GCD & LCM.cpp to Factorial & GCD & LCM.cpp

--- Factorial & GCD & LCM.cpp
+++ Factorial & GCD & LCM.cpp
@@ -8,13 +8,11 @@
 #define no cout << "NO" << endl;
 #define yes cout << "YES" << endl;
 #define Hero ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-const ll MAX_CHAR = 256;
 using namespace std;
 
 // Recursive function to return gcd of a and b.
 long long gcd(long long int a, long long int b){
-    if (b == 0)
-        return a;
+    if (b == 0) {return a;}
     return gcd(b, a % b);
 }
 
@@ -23,8 +21,19 @@ long long lcm(ll a, ll b){
     return (a / gcd(a, b)) * b;
 }
 
+// Function to calculate factorial of the number.
+long double factorial(ld n){
+    if (n == 0 || n == 1) {return 1;}
+    return n * factorial(n - 1);
+}
+
 int main() {
-    ll n1, n2; cin >> n1 >> n2;
+    ll int n1, n2;
+    ld n3;
+    cin >> n1 >> n2 >> n3;
     cout << gcd(n1, n2) << " " << lcm(n1, n2) << endl;
+    
+    ld result = factorial(n3);
+    cout << result << endl;
     return 0;
 }
